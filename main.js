34294401c61221adb3ba64360b7d88e197d53217
keyuanyuan <229497835@qq.com>
2018-04-24 17:05:53 +0800

    添加loading

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+setTimeout(function(){
+    siteWelcome.classList.remove('active')
+  },1000)
+  
 window.onscroll=function(xx) {
     if(window.scrollY>0) {
         topNavBar.classList.add('sticky')
